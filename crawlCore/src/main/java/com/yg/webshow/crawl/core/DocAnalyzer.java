c0968041c1ad679e698d042f4fbe89cb708fc38f
gonni <gonni21c@gmail.com>
2017-06-13 18:57:19 +0900

    DL4J library and sample added

---
+++ crawlCore/src/main/java/com/yg/webshow/crawl/core/DocAnalyzer.java
@@ -0,0 +1,18 @@
+package com.yg.webshow.crawl.core;
+
+import org.deeplearning4j.text.tokenization.tokenizerfactory.KoreanTokenizerFactory;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
+
+//import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
+
+public class DocAnalyzer {
+	
+	public static void main(String ... v) {
+//		TokenizerFactory t = new DefaultTokenizerFactory();
+//		
+		TokenizerFactory kt = new KoreanTokenizerFactory();
+//		
+//		KoreanTokenizer ktt = null ;
+
+	}
+}
