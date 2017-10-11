39db58774b9f0d4919ee019bc7fc8d50412b4655
gonni <gonni21c@gmail.com>
2017-10-11 20:19:55 +0900

    Daily Commit

--- crawlCore/src/main/java/com/yg/webshow/crawl/seeds/NewClienPark.java
+++ crawlCore/src/main/java/com/yg/webshow/crawl/seeds/NewClienPark.java
@@ -138,8 +138,11 @@ public class NewClienPark implements IDocWrapper {
 		
 		String testUrl = "https://www.clien.net/service/board/park/10720251";
 		testUrl = "https://www.clien.net/service/board/park/10719290";
+		testUrl = "https://www.clien.net/service/board/cm_car/11286165?po=0&od=T31&sk=&sv=&category=&groupCd=&articlePeriod=default";
+		
 		
 		WebDocBbs content = test.getContent(testUrl);
+		System.out.println("----------------------");
 		System.out.println("Contents >" + content);
 	}
 
